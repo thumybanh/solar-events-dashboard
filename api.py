abb647683c6cc1a36ee19b84776fbca379b96263
mybanh <mythubanh@gmail.com>
2026-03-11 14:04:39 -0500

    fixed the logic in date for parameters in api python

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import json
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
 
 app = FastAPI()
 
@@ -24,6 +25,10 @@ def get_events(start_date: str = None, end_date: str = None, goes_class: str = N
     # to check the range of dates where the gev appears. 
     if start_date and end_date: 
         events = [e for e in events if start_date.replace('-', '/') <= e['event_start'] <= end_date.replace('-', '/')] # the datepicker in the js is formatted with "-" but the json format is with "/" so we need to replace it
+    if start_date: 
+         events = [e for e in events if e['event_start'] >= start_date.replace('-', '/')]
+    if end_date: 
+         events = [e for e in events if e['event_end'] <= end_date.replace('-', '/')]
     if goes_class: 
         events = [e for e in events if e['event_GOES'].startswith(goes_class)]
     return events
@@ -39,3 +44,18 @@ def get_event(event_id: str):
         
     return {"error": "Event not found"}
 
+
+@app.get('/events/download/')
+def get_events(start_date: str = None, end_date: str = None, goes_class: str = None):
+    with open('events.json', 'r') as f:
+            events = json.load(f)
+
+    if start_date and end_date: 
+            events = [e for e in events if start_date.replace('-', '/') <= e['event_start'] <= end_date.replace('-', '/')] # the datepicker in the js is formatted with "-" but the json format is with "/" so we need to replace it
+    if start_date: 
+         events = [e for e in events if e['event_start'] >= start_date.replace('-', '/')]
+    if end_date: 
+         events = [e for e in events if e['event_end'] <= end_date.replace('-', '/')]
+    if goes_class: 
+            events = [e for e in events if e['event_GOES'].startswith(goes_class)]
+    return events
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState,useEffect } from "react";
 
-
 function App(){
   const[events, setEvents] = useState([]) // a state variable (events) and updated it by setEvents
   const[startDate, setStartDate] = useState('')
   const[endDate, setEndDate] = useState('')
   const[goes_class, setGoesClass] = useState('')
+  const[selectedEvent, setSelectedEvent] = useState(null)
+  const[imageURL, setImageURL] = useState(null)
 
 
-const fetchEvents = () => {
+const fetchEvents = () => { 
   let url = "http://localhost:8000/events"
 
   if(startDate && endDate){
@@ -65,15 +66,43 @@ const downloadCSV = () => {
   link.click()
 }
 
-// const imageURL = `https://api.helioviewer.org/v2/getJP2Image/?date=${date}&sourceId=${sourceId}`
+const downloadJSON = () => {
 
+  const JSONcontent = JSON.stringify(events)
 
+  const blob = new Blob([JSONcontent], {type: 'text/json'})
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement('a')
+  link.href = url
+  link.download = 'events.json'
+  link.click()
+}
+
+// const imageFetch = (event) => {
+//   setSelectedEvent(event)
+
+//   const sourceId = getSourceId(event.event_start)
+//   const date = helioDate(event.event_start)
+
+//   const imageURL = `https://api.helioviewer.org/v2/getJP2Image/?date=${date}&sourceId=${sourceId}`
+//   setImageURL(imageURL)
+// }
+
+const imageFetch = async (event) => {
+  setSelectedEvent(event)
+  const date = helioDate(event.event_start)
+
+  const response = await fetch(`https://api.helioviewer.org/v2/takeScreenshot/?date=${date}&imageScale=5&layers=[SDO,AIA,AIA,171,1,100]&x0=0&y0=0&width=512&height=512`) // [Observatory, Instrument, Detector, Measurement, Visible, Opacity]
+
+  const data = await response.json()
+  setImageURL(`https://api.helioviewer.org/v2/downloadScreenshot/?id=${data.id}`)
+}
 
 useEffect(()=>{
   fetchEvents()
 }, [])
 
-  // useEffect( () =>{ // runs when the page loads
+  // useEffect( () =>{ // runs when the page loads // change the image jp2 into png so that i can pop up 
   //   fetch("http://localhost:8000/events") //to call the fastapi backend
   //   .then(res => res.json()) // converts the response into json format
   //   .then(data => setEvents(data)) //save the events into state
@@ -91,6 +120,7 @@ useEffect(()=>{
       <input type="search" onChange={e=> setGoesClass(e.target.value)}/>
       <button onClick={fetchEvents}>Filter</button>
       <button onClick={downloadCSV}>Download CSV</button>
+      <button onClick={downloadJSON}>Download JSON</button>
       <table>
         <thead>
           <tr>
@@ -105,7 +135,7 @@ useEffect(()=>{
         <tbody>
           {events.map(event =>( //for every event in events, loop through to map each items into its corresponding rows
             <tr key = {event.event_id}>
-              <td>{event.event_id}</td>
+              <td  onClick={() => imageFetch(event)} style = {{cursor: 'pointer'}}>{event.event_id}</td>
               <td>{event.event_start}</td>
               <td>{event.event_stop}</td>
               <td>{event.event_peak}</td>
@@ -115,6 +145,20 @@ useEffect(()=>{
           ))}
         </tbody>
       </table>
+
+    {selectedEvent && (
+      <div style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.7)', display: "flex", justifyContent :'center',alignItems: 'center', zIndex: 1000 }}>
+        <div style={{backgroundColor: 'white', padding: '20px', borderRadius: '10px', maxWidth: '600px', width: '90%'}}>
+          <button onClick={()=> setSelectedEvent(null)}>X close</button>
+          <h2>{selectedEvent.event_id}</h2>
+          <p>Start: {selectedEvent.event_start}</p>
+          <p>GOES class:{selectedEvent.event_GOES} </p>
+          <p>Position: {selectedEvent.event_position}</p>
+          {imageURL && <img src={imageURL} style={{width: '100%'}}></img>}
+        </div>
+      </div>
+    )}
+
     </div>
   )
 }
